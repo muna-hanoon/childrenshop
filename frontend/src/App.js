ae1ece2f632a4b816757a87724730cd2e6dbbbc4
Muna <faik_muna@yahoo.com>
2022-08-16 19:39:11 -0400

    Kids-Shop-React

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Children Place</a>
+      </header>
+      <main>
+        lists products
+      </main>
+    </div>
+  );
+}
+
+export default App;
